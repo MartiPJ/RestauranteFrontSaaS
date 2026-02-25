80e877fdae9169c2d440260a45f80d1c867ff9c1
MartiPJ <mugiwara1209@gmail.com>
2026-02-25 14:06:45 -0600

    style: :lipstick: The title of the index html was updated

--- vite.config.ts
+++ vite.config.ts
@@ -7,15 +7,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3001,
-    // AÑADE ESTA CONFIGURACIÓN DE PROXY
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://localhost:3000', // Tu backend NestJS
-    //     changeOrigin: true,
-    //     secure: false,
-    //     rewrite: (path) => path.replace(/^\/api/, '/api'), // Mantiene el prefijo /api
-    //   },
-    // },
   },
 
   plugins: [vue(), tailwindcss()],
